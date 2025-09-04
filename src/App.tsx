fadd8068acf44c293b9fd798221eb6cdc7beb382
erica-monteiro-developer <00.erica.al.monteiro@gmail.com>
2025-09-04 00:11:28 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import './index.css'
+import { GridItem } from './components/GridItem';
+
+import { levels, calculateImc } from './helpers/imc';
+
+
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+
+    } else {
+      alert("Digite todos os campos.")
+    }
+  }
+
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex.: 1.5 (em metros)."
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex.: 75.3 (em kg)."
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          <div className={styles.grid}>
+            {levels.map((item, key) => (
+              <div key={key}>{item.title}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
